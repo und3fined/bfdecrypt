9f7427e719082c97c69290d84c4a1b2ce72def47
und3fined <me@und3fined.com>
2023-05-04 00:01:11 +0700

    save

--- prefs/bfdRootListController.m
+++ prefs/bfdRootListController.m
@@ -61,5 +61,5 @@ __attribute__((constructor))
 static void init(void)
 {
 	reloadPreferences();
-	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)reloadPreferences, CFSTR("com.level3tjg.bfdecrypt.settingschanged"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
+	CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), NULL, (CFNotificationCallback)reloadPreferences, CFSTR("dev.und3fy.bfdecrypt.settingschanged"), NULL, CFNotificationSuspensionBehaviorDeliverImmediately);
 }
