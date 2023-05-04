8e51fc573ddbc0c856062620108afb3ab5818727
und3fined <me@und3fined.com>
2023-05-05 00:55:15 +0700

    update more

--- AltListPrefs/APPRootListController.m
+++ AltListPrefs/APPRootListController.m
@@ -2,6 +2,42 @@
 
 @implementation APPRootListController
 
+void reloadPreferences()
+{
+	preferences = [NSDictionary dictionaryWithContentsOfFile:kAltListPrefsPlistPath];
+	NSMutableArray *bundles = [[NSMutableArray alloc] init];
+
+	// select preference from key in Root.plist
+	selectedApplications = [preferences objectForKey:@"selectedApplications"]
+
+	for (NSString *key in selectedApplications) {
+		if ([[preferences objectForKey:key] boolValue] == YES) {
+				[bundles addObject:key];
+		}
+	}
+
+	NSDictionary *filterDict = @{
+		@"Filter": @{
+			@"Bundles": bundles
+		}
+	};
+
+	@try
+	{
+		[filterDict writeToFile:@"/var/jb/Library/MobileSubstrate/DynamicLibraries/bfdecrypt_fromprefs.plist" atomically:NO];
+		[filterDict writeToFile:@"/var/jb/Library/MobileSubstrate/DynamicLibraries/bfdecrypt.plist" atomically:NO];
+	}
+	@catch(id anException) {
+		NSLog(@"[bfdecryptPrefs] Error writing preferences");
+		ShowAlert(@"Error writing preferences", @"Error");
+	}
+}
+
+- (void)syncSelectedApplications
+{
+	reloadPreferences();
+}
+
 - (NSArray *)specifiers {
 	if (!_specifiers) {
 		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
@@ -11,3 +47,9 @@
 }
 
 @end
+
+__attribute__((constructor))
+static void init(void)
+{
+	reloadPreferences();
+}
