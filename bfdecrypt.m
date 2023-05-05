6d4c71a4c3878215c7fb7dc86ad1b594c57ec49e
und3fined <me@und3fined.com>
2023-05-05 19:32:56 +0700

    fix syntax

--- bfdecrypt.m
+++ bfdecrypt.m
@@ -31,7 +31,7 @@ __attribute__ ((constructor)) static void bfinject_rocknroll() {
     NSLog(@"[bfdecrypt] Constructor called");
 
     NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
-    NSDictionary prefs = [NSDictionary dictionaryWithContentsOfFile:@"/var/mobile/Library/Preferences/dev.und3fy.bfdecrypt_prefs.plist"];
+    NSDictionary *prefs = [[NSDictionary alloc] initWithContentsOfFile:@"/var/jb/var/mobile/Library/Preferences/dev.und3fy.bfdecrypt_prefs.plist"];
 
     NSLog(@"[bfdecrypt] bundleID: %@", bundleID);
 
